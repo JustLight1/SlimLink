86e8805a127e248ef5759eeff68ae4a47fd79daa
Форов Александр <5h9t5@list.ru>
2024-07-30 19:20:34 +0300

    feat(models): added model URL_map

--- slimlink_app/models.py
+++ slimlink_app/models.py
@@ -1,7 +1,24 @@
-from datetime import datetime
+from datetime import datetime, timezone
 
-from slimlink_app import db
+from . import db
 
 
 class URL_map(db.Model):
-    ...
+    id = db.Column(db.Integer, primary_key=True)
+    original = db.Column(db.Text, nullable=False)
+    short = db.Colum(db.String(16), unique=True)
+    timestamp = db.Column(db.DateTime, index=True,
+                          default=datetime.now(timezone.utc))
+
+    def to_dict(self):
+        return dict(
+            id=self.id,
+            original=self.original,
+            short=self.short,
+            timestamp=self.timestamp
+        )
+
+    def from_dict(self, data):
+        for field in ['original', 'short']:
+            if field in data:
+                setattr(self, field, data[field])
