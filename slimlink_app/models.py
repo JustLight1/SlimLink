148b5f86249b022020ec80347544f04587e99180
Форов Александр <5h9t5@list.ru>
2024-07-31 20:22:00 +0300

    feat(models): update func to_dict and remove unused func from_dict

--- slimlink_app/models.py
+++ slimlink_app/models.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timezone
 
+from flask import url_for
+
 from . import db
 
 
@@ -11,14 +13,8 @@ class URL_map(db.Model):
                           default=datetime.now(timezone.utc))
 
     def to_dict(self):
-        return dict(
-            id=self.id,
-            original=self.original,
-            short=self.short,
-            timestamp=self.timestamp
-        )
-
-    def from_dict(self, data):
-        for field in ['original', 'short']:
-            if field in data:
-                setattr(self, field, data[field])
+        return {
+            'url': self.original,
+            'short_link': url_for('redirect_to_original',
+                                  short_id=self.short, _external=True),
+        }
