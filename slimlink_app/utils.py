d68e2a1771ed4ab411b58ed757d1986229d02cb1
Форов Александр <5h9t5@list.ru>
2024-07-31 20:23:58 +0300

    feat(utils): added func check_custom_id: -> check users short_link for valid  symbols

--- slimlink_app/utils.py
+++ slimlink_app/utils.py
@@ -9,3 +9,10 @@ def get_unique_short_id():
     link = (''.join([random.choice(list(ALPHANUMERIC_CHARACTERS))
             for x in range(6)]))
     return link
+
+
+def check_custom_id(custom_id):
+    for symbol in custom_id:
+        if symbol not in ALPHANUMERIC_CHARACTERS:
+            return False
+    return True
